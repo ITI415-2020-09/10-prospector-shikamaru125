1b93105c9e18ea429ed6f2012737ebd59cb781d0
shikamaru125 <70780812+shikamaru125@users.noreply.github.com>
2020-12-13 13:07:14 -0500

    Finalized

--- Assets/__Scripts/CardProspector.cs
+++ Assets/__Scripts/CardProspector.cs
@@ -16,6 +16,12 @@ public class CardProspector : Card
     public List<CardProspector> hiddenBy = new List<CardProspector>();
     public int layoutID;
     public SlotDef slotDef;
+
+    public override void OnMouseUpAsButton()
+    {
+        Prospector.S.CardClicked(this);
+        base.OnMouseUpAsButton();
+    }
     // Start is called before the first frame update
     void Start()
     {
